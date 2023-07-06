70570bc078f944e51bdb90225cf94bbb2fe9049d
marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2023-07-06 12:53:01 +0100

    ???

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marcoj18.github.io',
-  base: '/porfolio.io',
+  site: 'https://marcoj18.github.io/porfolio.io',
   integrations: [tailwind()]
 });
